826cf2e7efe5dc2d4331fa0261528e986e9c1ae2
James Wahlin <james@mongodb.com>
2018-07-18 10:25:45 -0400

    stitch w/ functions

--- ticker.py
+++ ticker.py
@@ -1,25 +1,15 @@
 #!/usr/bin/env python
 
 import time
-
 import requests
 import scrolling_text
-
-def get_ticker_list():
-    url = "https://stitch.mongodb.com/api/client/v1.0/app/mdb-ticker-hplox/svc/ConfigView/incomingWebhook/59f8b7ae058429a3914d6f15"
-    response = requests.get(url).json()
-    data = response["tickerList"]
-
-    #print(data)
-
-    return data.split(",")
 
 def get_quote(ticker):
 
     has_response = False
     while not has_response:
-        url = "https://stitch.mongodb.com/api/client/v1.0/app/mdb-ticker-hplox/svc/GetQuoteForTicker/incomingWebhook/59f9d66046224c60567145be"
-        payload = '{"ticker": "' + ticker + '"}'
+        url = "ADD URL HERE"
+        payload = '{"ticker": "' + ticker + '"}';
         response = requests.post(url, data=payload, headers={"Content-Type": "application/json"})
 
         if not response.status_code == 200:
@@ -27,23 +17,15 @@ def get_quote(ticker):
             continue
 
         has_response = True
-        data = response.json()["body"]
+        quote = response.json()
 
-        if data.find("Error Message") >= 0:
-            raise ValueError("Invalid ticker: " + ticker)
+        last_price = float(quote["price"])
+        volume = int(quote["volume"])
 
-    daily_quotes = data.splitlines()
+        prev_close_price = float(quote["prevPrice"])
+        percent_change = 100 * ((last_price - prev_close_price) / prev_close_price)
 
-    current_quote = daily_quotes[1].split(",")
-    previous_quote = daily_quotes[2].split(",")
-
-    close_price = float(current_quote[4])
-    volume = int(current_quote[5])
-
-    prev_close_price = float(previous_quote[4])
-    percent_change = 100 * ((close_price - prev_close_price) / prev_close_price)
-
-    return (close_price, volume, prev_close_price, percent_change)
+    return (last_price, volume, prev_close_price, percent_change)
 
 def run():
     color_ticker = (255, 255, 255)
@@ -52,33 +34,31 @@ def run():
     color_down = (255, 0, 0)
 
     while True:
-        ticker_list = get_ticker_list()
-
-        if len(ticker_list) == 1 and ticker_list[0] == "pause":
-            time.sleep(1)
-            continue
         try:
-            for ticker in ticker_list:
-                last_price, volume, prev_close_price, percent_change = get_quote(ticker)
+            last_price, volume, prev_close_price, percent_change = get_quote("mdb")
 
-                display_last_price = "{:.2f}".format(last_price)
-                display_volume = "{:,}".format(volume)
-                display_percent_change = "{0:.2f}".format(percent_change)
+            display_last_price = "{:.2f}".format(last_price)
+            display_volume = "{:,}".format(volume)
+            display_percent_change = "{0:.2f}".format(percent_change)
 
-                lines = [ticker, display_last_price, "%chg " + display_percent_change,
-                         "vol " + display_volume]
+            lines = ["mdb", display_last_price, "%chg " +  display_percent_change,
+                     "vol " + display_volume]
 
-                price_color = color_flat
-                if last_price > prev_close_price:
-                    price_color = color_up
-                elif last_price < prev_close_price:
-                    price_color = color_down
+            price_color = color_flat
+            if last_price > prev_close_price:
+                price_color = color_up
+            elif last_price < prev_close_price:
+                price_color = color_down
 
-                colors = [color_ticker, price_color, price_color, price_color]
+            colors = [color_ticker, price_color, price_color, price_color]
 
-                scrolling_text.display_text(lines, colors)
+            scrolling_text.display_text(lines, colors)
         except ValueError as valerr:
             print(valerr)
+            time.sleep(1)
+            continue
+        except Exception as e:
+            print (e)
             time.sleep(1)
             continue
 
